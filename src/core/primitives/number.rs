4e6609d59e09a4687528ccbcf3ba41a38475b4c3
Judd Keppel <kepzorz@gmail.com>
2020-08-12 16:19:46 -0700

    implement dereferencing for number type

--- src/core/primitives/number.rs
+++ src/core/primitives/number.rs
@@ -2,7 +2,23 @@ use crate::Result;
 use orga::{Decode, Encode};
 use rust_decimal::Decimal;
 use std::io::{Read, Write};
+use std::ops::{Deref, DerefMut};
+
 pub struct Number(Decimal);
+
+impl Deref for Number {
+    type Target = Decimal;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Number {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 impl Encode for Number {
     fn encode_into<W: Write>(&self, dest: &mut W) -> Result<()> {
@@ -30,11 +46,11 @@ mod tests {
     #[test]
     fn encode_decode() {
         let n = Number(9.into());
-        let n_str = n.0.to_string();
+        let n_str = n.to_string();
         assert_eq!(n_str, "9");
-        let n = Number(n.0.checked_div(2.into()).unwrap());
+        let n = Number(n.checked_div(2.into()).unwrap());
         let n_bytes = n.encode().unwrap();
         let n = Number::decode(n_bytes.as_slice()).unwrap();
-        assert_eq!(n.0.to_string(), "4.5");
+        assert_eq!(n.to_string(), "4.5");
     }
 }
