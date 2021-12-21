ac0da18364773151ce6fe6d97988d17a506ab54d
Charlie Little <cwlittle@utexas.edu>
2021-12-21 17:44:57 -0600

    Add Bitcoin error variants

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,13 @@ pub enum Error {
     #[error(transparent)]
     Bitcoin(#[from] bitcoin::util::Error),
     #[error(transparent)]
+    BitcoinEncode(#[from] bitcoin::consensus::encode::Error),
+    #[error(transparent)]
     BitcoinRpc(#[from] bitcoincore_rpc::Error),
     #[error("{0}")]
     Header(String),
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
     #[error(transparent)]
     Orga(#[from] orga::Error),
     #[error("{0}")]
