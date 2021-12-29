5dcaaefa2d4905bd8c27dab05c8dbc10336852ef
Charlie Little <cwlittle@utexas.edu>
2021-12-29 12:04:58 -0600

    Revert "Update Relayer tests for async logic"
    This reverts commit be31a813a439cd72d354cd18bb435daec15041a9.

--- tests/relayer.rs
+++ tests/relayer.rs
@@ -1,43 +1,29 @@
-#![cfg(todo)]
-
 use bitcoincore_rpc::{Auth, Client as BtcClient, RpcApi};
 use bitcoind::BitcoinD;
-use nomic::bitcoin::adapter::Adapter;
-use nomic::bitcoin::header_queue::{Config, HeaderQueue};
+use nomic::app::App;
 use nomic::bitcoin::relayer::Relayer;
-use orga::encoding::Encode;
-use orga::store::{MapStore, Shared, Store};
+use orga::prelude::*;
+use std::thread;
 
-#[test]
-fn relayer() {
+pub fn app_client() -> TendermintClient<App> {
+    TendermintClient::new("http://localhost:26657").unwrap()
+}
+
+#[tokio::test]
+async fn relayer() {
     let bitcoind = BitcoinD::new(bitcoind::downloaded_exe_path().unwrap()).unwrap();
-
-    let address = bitcoind.client.get_new_address(None, None).unwrap();
-    bitcoind.client.generate_to_address(32, &address).unwrap();
-    let trusted_hash = bitcoind.client.get_block_hash(32).unwrap();
-    let trusted_header = bitcoind.client.get_block_header(&trusted_hash).unwrap();
 
     let bitcoind_url = bitcoind.rpc_url();
     let bitcoin_cookie_file = bitcoind.params.cookie_file.clone();
     let rpc_client = BtcClient::new(&bitcoind_url, Auth::CookieFile(bitcoin_cookie_file)).unwrap();
 
-    let encoded_header = Encode::encode(&Adapter::new(trusted_header)).unwrap();
-    let mut config: Config = Default::default();
-    config.encoded_trusted_header = encoded_header;
-    config.trusted_height = 32;
-    config.retargeting = false;
+    thread::spawn(move || Node::<App>::new(".relayer-integration").reset().run());
 
-    bitcoind
-        .client
-        .generate_to_address(10 as u64, &address)
-        .unwrap();
+    thread::sleep(std::time::Duration::from_secs(2));
 
-    let store = Store::new(Shared::new(MapStore::new()));
+    let app_client = app_client();
+    let relayer = Relayer::new(rpc_client, app_client);
 
-    let mut header_queue = HeaderQueue::with_conf(store, Default::default(), config).unwrap();
-    let relayer = Relayer::new(rpc_client);
-    relayer.bounded_listen(10, &mut header_queue).unwrap();
-    let height = header_queue.height().unwrap();
-
-    assert_eq!(height, 42);
+    let height = relayer.app_trusted_height().await.unwrap();
+    assert_eq!(height, 0);
 }
